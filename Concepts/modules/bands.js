ccc59de0778993b20bd0c41cc9b60c3284fb7c19
tucksa <tuckerjsarah@gmail.com>
2020-03-01 19:02:36 -0500

    modules

---
+++ Concepts/modules/bands.js
@@ -0,0 +1,8 @@
+// You can modularize your code with node.js using the module package. 
+//In this file I will define an object (bands) and export it to be used byt the output file
+module.exports = {
+    classic : 'Led Zeplin',
+    punk : 'Green Day',
+    rap : 'Tupac', 
+    classical : 'Beethoven'
+}
---
+++ Concepts/modules/output.js
@@ -0,0 +1,24 @@
+//Using 'require' and the path I now have access to the band object and can do what i want with it
+const bands = require('./bands')
+
+//Accept an argument for what type of band
+let type = process.argv[2]
+
+//switch cases for different types to get output
+switch(type){
+    case'classic' :
+        console.log(bands.classic + ' is a classic band')
+        break;
+    case 'rap':
+        console.log(bands.rap + ' is a rap band')
+        break;
+    case 'punk':
+        console.log(bands.punk + ' is a punk band')
+        break;
+    case 'classical':
+        console.log(bands.classical + ' is a classical band')
+        break;
+    default:
+        console.log('sorry, you input a band type we do not have information on')
+}
+

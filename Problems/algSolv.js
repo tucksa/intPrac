88eb03a28b23d2506954068d04015ece9458cbbe
tucksa <tuckerjsarah@gmail.com>
2020-03-03 13:23:02 -0500

    practice problem 1

--- Problems/algSolv.js
+++ Problems/algSolv.js
@@ -1,3 +1,23 @@
 // Using node.js take arguments for operator, num 1 and num 2 and return the result
+let operator = process.argv[2]
+let arg1 = parseFloat(process.argv[3])
+let arg2 = parseFloat(process.argv[4])
+
+switch (operator) {
+    case 'add':
+        console.log(arg1 + arg2)
+        break;
+    case 'subtract':
+        console.log(arg1 - arg2)
+        break;
+    case 'multiply':
+        console.log(arg1*arg2)
+        break;
+    case 'divide':
+        console.log(arg1/arg2)
+        break;
+    default:
+        console.log('Sorry, something went wrong... check your inputs are valid (add/subtract/multiply/divide)')
+}
 
 //Bonus, take argument 'algebra' and a basic expression lik 4x+2 = 10 and return the solution for x

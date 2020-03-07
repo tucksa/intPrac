aefbfe16d5cb102b3c4aed89bc50382bf961069a
tucksa <tuckerjsarah@gmail.com>
2020-03-07 18:10:44 -0500

    cleaning up

--- Problems/algSolv.js
+++ Problems/algSolv.js
@@ -21,7 +21,7 @@ switch (operator) {
 }
 
 //Bonus, take argument 'algebra' and a basic expression lik 4x+2 = 10 and return the solution for x
-let equation = process.argv[3]
+const equation = process.argv[3]
 //Isolate the x on the left side
 
 //split expresion at = to get right and left side of equation
@@ -38,42 +38,57 @@ for(let i = 0; i < rightLeftArr[0].length; i ++){
         algOperator.push(rightLeftArr[0][i])
     }
 }
+function popX(val){
+    let num = val.split('')
+    num.pop()
+    num = num.join()
+    return num
+}
 
 for(let i =0; i < algOperator.length; i ++){
     if(leftArr[i].includes('x')){
         if(algOperator[i] == '-'){
             leftArr[i+1]= parseInt(leftArr[i+1])*(-1)
-            xVals.push(leftArr[i])
-        }else if(algOperator[i] == '/'){
-            xVals.push(leftArr[i]+'/'+ leftArr[i+1])
+            let val = popX(leftArr[i])
+            xVals.push(val)
+        }else if(algOperator[i] == '/' && !leftArr[i+1].includes('x')){
+            let val = popX(leftArr[i])
+            xVals.push(val+'/'+ leftArr[i+1])
         }else if(algOperator[i] == '*'){
             if(leftArr[i+1].includes('x')){
-                let coreNum1 = leftArr[i].split('')
-                coreNum1.pop()
-                coreNum1= coreNum1.join('')
-                let coreNum2 = leftArr[i+1].split('')
-                coreNum2.pop()
-                coreNum2 = coreNum2.join('')
-                xVals.push(parseInt(coreNum1) * parseInt(coreNum2) + 'x')
+                let coreNum1 = popx(leftArr[i])
+                let coreNum2 = popx(leftArr[i+1])
+                xVals.push(parseInt(coreNum1) * parseInt(coreNum2))
             }else{
-                let coreNum1 = leftArr[i].split('')
-                coreNum1.pop()
-                coreNum1.join('')
-                xVals.push(parseInt(coreNum1) * parseInt(leftArr[i+1]) + 'x')
+                let coreNum1 = popx(leftArr[i])
+                xVals.push(parseInt(coreNum1) * parseInt(leftArr[i+1]))
             }
         }else{
             if(leftArr[i+1].includes('x')){
-                let coreNum1 = leftArr[i].split('')
-                coreNum1.pop()
-                coreNum1 = coreNum1.join('')
-                let coreNum2 = leftArr[i+1].split('')
-                coreNum2.pop()
-                coreNum2 = coreNum2.join('')
-                xVals.push(parseInt(coreNum1) + parseInt(coreNum2) + 'x')
+                let coreNum1 = popx(leftArr[i])
+                let coreNum2 = popx(leftArr[i+1])
+                xVals.push(parseInt(coreNum1) + parseInt(coreNum2))
             }else{
-                xVals.push(leftArr[i])
+                xVals.push(popx(leftArr[i]))
             }
         }
+    }else if(leftArr[i+1].includes('x') && !leftArr[i].includes('x')){
+        if(algOperator[i] == '-'){
+            xVals.push('-' + popx(leftArr[i+1]))
+        }else if(algOperator[i] == '/'){
+            xVals.push(leftArr[i]+ '/' + popx(leftArr[i+1]))
+        }else if(algOperator[i] == '*'){
+            let coreNum = popx(leftArr[i+1])           
+            xVals.push(parseInt(leftArr[i])*parseInt(coreNum))
+        }else{
+            let coreNum = popx(leftArr[i+1])
+            xVals.push(parseInt(leftArr[i])+parseInt(coreNum))
+        }
+    }else{
+        //add values
+        //sub values
+        //mult values
+        //divid values
     }
 }
 //if i inludes 'x' then set this equal to isolated
